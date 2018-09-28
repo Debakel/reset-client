2c2981cffe8a3fd3c814b7bd10084288ebc2f64f
Moritz <git@mrtz.me>
2018-09-29 00:26:08 +0200

    Move web socket client to client/

--- src/app/client/websocket.client.ts
+++ src/app/client/websocket.client.ts
@@ -1,6 +1,6 @@
 import {Observable} from 'rxjs';
 
-export class WebsocketClient {
+export class WebSocketClient {
   onMessage: Observable<MessageEvent>;
   onOpen: Observable<any>; // TODO: Add type
   onClose: Observable<any>; // TODO: Add type
@@ -10,14 +10,13 @@ export class WebsocketClient {
 
   constructor(url: string) {
     this.socket = new WebSocket(url);
-
     this.onMessage = new Observable(observer => this.socket.onmessage = (data) => observer.next(data));
     this.onOpen = new Observable(observer => this.socket.onopen = (data) => observer.next(data));
     this.onClose = new Observable(observer => this.socket.onclose = (data) => observer.next(data));
     this.onError = new Observable(observer => this.socket.onerror = (data) => observer.next(data));
   }
 
-  public send(message: string) {
+  public send(message: any) {
     this.socket.send(message);
   }
 }
