f3808b98da2df1afc82d9cf1e4da1de7faa3c87d
Felix Kampfer <felix.kampfer@hs-augsburg.de>
2018-09-28 23:42:36 +0200

    added pixi to angular (and the people who read commit messages in their entirety are silly, beautiful people!)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { GameAreaComponent } from './game-area/game-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameAreaComponent
   ],
   imports: [
     BrowserModule,
