52a2aab16d96361648bf42e1ff81fe92dcc157bf
Joachim Eckerlin <eckerlin@team23.de>
2018-09-28 21:14:46 +0200

    A moving man

--- src/assets/canvas.js
+++ src/assets/canvas.js
@@ -1,6 +1,31 @@
 class Canvas {
     constructor() {
         this.app = new PIXI.Application({});
+        let worker = PIXI.loader.add('assets/gameassets/worker_walking.json').load(this.setup.bind(this));
+    }
+
+    setup() {
+        let sheet = PIXI.loader.resources["assets/gameassets/worker_walking.json"].spritesheet;
+        console.log(sheet);
+
+        var frames = [];
+
+        for (var i = 0; i < 30; i++) {
+
+            // magically works since the spritesheet was loaded with the pixi loader
+            frames.push(PIXI.Texture.fromFrame('worker_walking' + i%4 + '.png'));
+        }
+
+
+        const worker = new PIXI.extras.AnimatedSprite(frames);
+        worker.anchor.set(0.5);
+        worker.x = this.app.screen.width / 2;
+        worker.y = this.app.screen.height / 2;
+
+        worker.animationSpeed = .1;
+        worker.play();
+
+        this.app.stage.addChild(worker);
     }
 
     render()  {
